c6df971423436c235c201a06adc0e665af244491
Alain LESAGE <alain.lesage@etu.udamail.fr>
2014-03-10 16:21:17 +0100

    Edition Pojos

---
+++ src/java/ORM/DataBase.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ORM;
+
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alainlesage
+ */
+public class DataBase {
+    
+    private String url = "jdbc:mysql://localhost:3306/projet_web_jee";
+    
+    private String user = "admin";
+    
+    private String password = "lenfant";
+    
+    private Connection connexion = null;
+    
+    public DataBase(){
+        importDriver();
+        connect();
+    }
+    
+    @Override
+    public void finalize() throws Throwable
+    {
+        disconnect();
+        super.finalize();
+    }
+
+    private void importDriver() {
+        /* Load JDBC driver for MySQL */
+            try {
+                Class.forName( "com.mysql.jdbc.Driver" );
+            } catch ( ClassNotFoundException e ) {
+                /* Gérer les éventuelles erreurs ici. */
+            }
+    }
+
+    private void connect() {
+        /* Connexion à la base de données */
+            
+            try {
+                connexion = DriverManager.getConnection( url, user, password );
+
+                out.println("Connection established for user " + user);
+
+            } catch ( SQLException e ) {
+                out.println("Connection denied" + " " + e.getMessage() + " " + e.getSQLState());
+            } finally {
+                if ( connexion != null )
+                    disconnect();
+            }
+    }
+
+    private void disconnect() {
+        try {
+            /* Fermeture de la connexion */
+            connexion.close();
+        } catch ( SQLException ignore ) {
+            /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+        }
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -4,6 +4,9 @@
     Author     : alainlesage
 --%>
 
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.SQLException"%>
+<%@page import="java.sql.Connection"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -12,6 +15,36 @@
         <title>Blog</title>
     </head>
     <body>
+        <%
+            /* Chargement du driver JDBC pour MySQL */
+            try {
+                Class.forName( "com.mysql.jdbc.Driver" );
+            } catch ( ClassNotFoundException e ) {
+                /* Gérer les éventuelles erreurs ici. */
+            }
+            
+            /* Connexion à la base de données */
+            String url = "jdbc:mysql://localhost:3306/projet_web_jee";
+            String utilisateur = "admin";
+            String motDePasse = "lenfant";
+            Connection connexion = null;
+            try {
+                connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+
+                out.println("connexion OK pour l'utilisateur " + utilisateur);
+
+            } catch ( SQLException e ) {
+                out.println("connexion KO" + e.getMessage() + e.getSQLState());
+            } finally {
+                if ( connexion != null )
+                    try {
+                        /* Fermeture de la connexion */
+                        connexion.close();
+                    } catch ( SQLException ignore ) {
+                        /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+                    }
+            }
+        %>
         <%@ include file="header.jsp" %>
         <div>
             <%@ include file="article.jsp" %>
